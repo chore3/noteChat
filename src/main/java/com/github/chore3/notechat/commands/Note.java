142569a862dd9abd9eaa6124075eb6252fd3d939
chore_3 <anon408chore@gmail.com>
2026-02-03 12:21:16 +0900

    add: noteコマンドのひな形を作成

---
+++ src/main/java/com/github/chore3/notechat/commands/Note.java
@@ -0,0 +1,31 @@
+package com.github.chore3.notechat.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.MessageArgument;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.server.players.PlayerList;
+
+public class Note {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        var note = Commands.literal("note")
+                .requires((CommandSourceStack source) -> source.hasPermission(2)
+            );
+
+        var info = Commands.literal("info");
+        var warn = Commands.literal("warn");
+        var alert = Commands.literal("alert");
+
+        var message = Commands.argument("message", MessageArgument.message())
+                .executes(context -> {
+                    MessageArgument.resolveChatMessage(context, "message", (resolvedMessage) -> {
+                        CommandSourceStack commandsourcestack = context.getSource();
+                        PlayerList playerlist = commandsourcestack.getServer().getPlayerList();
+                        playerlist.broadcastChatMessage(resolvedMessage, commandsourcestack, ChatType.bind(ChatType.SAY_COMMAND, commandsourcestack));
+                    });
+                    return 1;
+                }
+            );
+    }
+}
