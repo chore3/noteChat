8962415cc23471efc9f4776f9ab49d5df55bb770
chore_3 <anon408chore@gmail.com>
2026-02-04 20:02:07 +0900

    change: 仕様変更に伴いregister内でのプログラムの利用方法を変更

--- src/main/java/com/github/chore3/notechat/commands/Note.java
+++ src/main/java/com/github/chore3/notechat/commands/Note.java
@@ -45,32 +45,13 @@ public class Note {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         var note = Commands.literal("note")
                 .requires((CommandSourceStack source) -> source.hasPermission(2));
-        
-        var info = Commands.literal("info");
-        var warn = Commands.literal("warn");
-        var alert = Commands.literal("alert");
 
-        var message = Commands.argument("message", StringArgumentType.greedyString()).executes((context) -> {
-            CommandSourceStack commandsourcestack = context.getSource();
-            PlayerList playerlist = commandsourcestack.getServer().getPlayerList();
-            String strMsg = StringArgumentType.getString(context, "message");
-            MutableComponent component = Component.empty()
-                    .append(Component.literal("--------------------\n").withStyle(ChatFormatting.GRAY))
-                    .append(Component.literal("ℹ Info\n").withStyle(ChatFormatting.AQUA))
-                    .append(Component.literal(strMsg + "\n").withStyle(ChatFormatting.LIGHT_PURPLE))
-                    .append(Component.literal("--------------------").withStyle(ChatFormatting.GRAY));
-
-            int i = 0;
-            for(ServerPlayer serverplayer : playerlist.getPlayers()) {
-                serverplayer.sendSystemMessage(component, false);
-            }
-
-            return i;
-        });
-
-        note.then(info.then(message))
-                .then(warn.then(message))
-                .then(alert.then(message));
+        var messageInfo = Commands.argument("message", StringArgumentType.greedyString())
+                .executes(ctx -> sendNote(ctx, NoteType.INFO));
+        var messageWarn = Commands.argument("message", StringArgumentType.greedyString())
+                .executes(ctx -> sendNote(ctx, NoteType.WARN));
+        var messageAlert = Commands.argument("message", StringArgumentType.greedyString())
+                .executes(ctx -> sendNote(ctx, NoteType.ALERT));
 
         dispatcher.register(note);
     }
