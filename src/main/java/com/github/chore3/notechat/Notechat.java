92023aab9009e0d21a481ee7cb081e2e23d014e4
chore_3 <anon408chore@gmail.com>
2026-02-04 19:09:28 +0900

    fix: LOGGERのスコープを修正

--- src/main/java/com/github/chore3/notechat/Notechat.java
+++ src/main/java/com/github/chore3/notechat/Notechat.java
@@ -10,13 +10,14 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Notechat.MODID)
 public class Notechat {
 
     public static final String MODID = "notechat";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
     @SuppressWarnings("removal")
     public Notechat() {
