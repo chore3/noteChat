e4a653abac4805c5f474c5015219ae6867cc9d1e
chore_3 <anon408chore@gmail.com>
2026-02-04 19:44:04 +0900

    change: NoteType列挙型とsendNoteメソッドを追加

--- src/main/java/com/github/chore3/notechat/commands/Note.java
+++ src/main/java/com/github/chore3/notechat/commands/Note.java
@@ -1,6 +1,7 @@
 package com.github.chore3.notechat.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -11,6 +12,36 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
 
 public class Note {
+    enum NoteType {
+        INFO("ℹ Info", ChatFormatting.AQUA),
+        WARN("⚠ Warn", ChatFormatting.GOLD),
+        ALERT("‼ Alert", ChatFormatting.RED);
+
+        final String title;
+        final ChatFormatting color;
+
+        NoteType(String title, ChatFormatting color) {
+            this.title = title;
+            this.color = color;
+        }
+    }
+
+    private static int sendNote(CommandContext<CommandSourceStack> context, NoteType type) {
+        String msg = StringArgumentType.getString(context, "message");
+        MutableComponent component = Component.empty()
+                .append(Component.literal("--------------------\n").withStyle(ChatFormatting.GRAY))
+                .append(Component.literal(type.title + "\n").withStyle(type.color))
+                .append(Component.literal(msg + "\n").withStyle(ChatFormatting.LIGHT_PURPLE))
+                .append(Component.literal("--------------------").withStyle(ChatFormatting.GRAY));
+        PlayerList list = context.getSource().getServer().getPlayerList();
+        int i = 0;
+        for (ServerPlayer player : list.getPlayers()) {
+            player.sendSystemMessage(component, false);
+            i++;
+        }
+        return i;
+    }
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         var note = Commands.literal("note")
                 .requires((CommandSourceStack source) -> source.hasPermission(2));
